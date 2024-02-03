22f8dad689f09fa3400398e175f2344a39a4c070
Divyanshu Saini <112510115+divyanshsaini962@users.noreply.github.com>
2024-02-04 01:23:04 +0530

    add a repo

---
+++ app/components/Banner.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <>
+      <div className='w-full h-96 justify-center items-center pt-12 text-center bg-purple-500'>
+            <h1 className='text-white text-7xl'>We are working</h1>
+            <h2 className='text-white text-3xl'>Saini Studio</h2>
+
+      </div>
+    </>
+  )
+}
+
+export default Banner
