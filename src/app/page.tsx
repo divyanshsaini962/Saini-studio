007ced419675465b617feefdfd9cb02794df58e3
Divyanshu Saini <112510115+divyanshsaini962@users.noreply.github.com>
2024-02-25 01:13:12 +0530

    contact path change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import Header from "../components/Header";
 import Footer from "../components/Footer"
 import Banner from "../components/Banner"
-import Contact from "../pages/contact"
+
 
 export default function Home() {
   return (
